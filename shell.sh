8eea4c7ea052fcbe67e1bf8456cc05208854ff45
Jang Min Sung <88541670+masterjms@users.noreply.github.com>
2024-12-27 17:43:05 +0900

    shell마무리

--- shell.sh
+++ shell.sh
@@ -17,6 +17,7 @@ var2=$2
 echo "username : ${var1:=Guest}"
 echo "greeting : ${var2:=welcome}" # 입력값이 없으면 guest와 welcome을 출력한다.
 
+
 #오류시 메세지 ${변수:?에러메세지}
 #!/bin/bash
 str1="str1"
@@ -24,3 +25,76 @@ str2=""
 error_msg="sorry"
 echo ${str1:?$error_msg}
 echo ${str2:?$error_msg}
+
+
+# if문법
+#!/bin/bash
+v1=$1
+v2=$2
+if [ $v1 -lt $v2 ]
+then
+        echo "v1 < v2"
+elif [ $v1 -gt $v2 ]
+then
+        echo "v1 > v2"
+else
+        echo "v1 = v2"
+fi
+
+
+# case문법
+# $변수 in 조건값) 수행문;; *)조건외 수행문 esac
+case $1 in
+        hi)
+                echo hi
+                ;;
+        hello)
+                echo hello
+                ;;
+        *)
+esac
+
+
+# for반복문
+# for 변수 in 범위
+for num in 1 2 3 4 5
+do
+        echo $num;
+done
+
+#for예시 - home경로의 모든 파일명을 가져온다.
+for file in $HOME/*
+do
+        echo $file;
+done
+
+#for예시 - {시작..끝..증가값}
+for num in {1..10..2}
+do
+        echo $num;
+done # 1부터 2씩 증가하며 10까지 반복한다.
+
+#for예시 - 배열
+arr=("a","b","c")
+for str in {arr[@]}
+do
+        echo $str;
+done
+
+
+#while문
+n=1
+while [ $n -lt 5 ]
+do
+    echo $n
+    n=$((n+1))
+done
+
+
+#전체실습
+echo "파일위치: $PWD"
+for num in {1..100}
+do
+        echo "파일명 : TABA_7_file_$num.txt";
+        echo "파일내용 : 이 파일은 TABA_7_file_$num.txt입니다.";
+done
